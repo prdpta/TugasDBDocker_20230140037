529b2bdd98acfcc4ee851d2e82a2de73ef6e6609
prdpta <pradiptaputramei01@gmail.com>
2026-04-21 14:25:21 +0700

    implementasi JPA Entity User dengan mapping tabel database spesifik

---
+++ src/main/java/com/praktikum08/deploy/model/User.java
@@ -0,0 +1,21 @@
+package com.praktikum08.deploy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name= "20230140037_users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String id;
+    @Column (nullable = false)
+    private String name;
+    @Column (nullable = false, unique = true)
+    private String nim;
+}
