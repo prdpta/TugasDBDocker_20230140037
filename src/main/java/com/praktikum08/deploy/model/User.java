0bfdf94fa42e7de241fe0630ff570e007dd98a33
prdpta <pradiptaputramei01@gmail.com>
2026-04-21 15:06:07 +0700

    menambahkan entitas User dengan Primary Key dan mapping tabel 20230140037_users

--- src/main/java/com/praktikum08/deploy/model/User.java
+++ src/main/java/com/praktikum08/deploy/model/User.java
@@ -2,6 +2,7 @@ package com.praktikum08.deploy.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,9 +14,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+
+    @Id
     private String id;
-    @Column (nullable = false)
+
+    @Column(nullable = false)
     private String name;
-    @Column (nullable = false, unique = true)
+
+    @Column(nullable = false, unique = true)
     private String nim;
 }
